b278bc43b1daadec017cfcd063369de41b72d84e
JoshuaDRose <joshuarose099@gmail.com>
2022-09-28 17:38:07 +1000

    Added enemy class

---
+++ libs/enemy.py
@@ -0,0 +1,13 @@
+import pygame
+from .util import *
+from .consts import *
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, image, position, groups):
+        super().__init__(groups)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = position
+        self.position = pygame.math.Vector2(self.rect.x, self.rect.y)
+
+
